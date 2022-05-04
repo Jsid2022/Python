cf7678624bc4bb97566c94aa15474e10eb5b8cce
Siddharth <vasujain8998@gmail.com>
2022-05-04 08:33:41 +0530

    Game TicTacToe

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,4 +1,5 @@
-from random import randint, choice
+from random import randint
+
 
 class TicTacToe:
 
@@ -8,117 +9,129 @@ class TicTacToe:
                       ['-', '-', '-'],
                       ['-', '-', '-'],
                     ]
+        self.valid_inputs = [1, 2, 3]
         self.possible_cords = [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3], [3, 1], [3, 2], [3, 3]]
 
 
     def print_board(self):
-        """This function will print the board."""
+        """This Function will Print the board."""
         for row in self.board:
-            for col in row:
-                print(f"{col}", end="  ")
+            for slot in row:
+                print(slot, end="  ")
             print()
 
 
     def player_cords(self):
-        """Get Player's coordinates."""
-        while True:
-            row = int(input("Enter Row Number: "))
-            if row >= 1 and row <= 3: break
-            else: print("Invalid Input!\nSelect from 1 to 3.\n")
-        
-        while True:
-            col = int(input("Enter Column Number: "))
-            if col >= 1 and col <= 3: break
-            else: print("Invalid Input!\nSelect from 1 to 3.\n")
-
-        coords = (row, col)
-        return list(coords)
-
-
-    def random_turn(self):
         """
-        Function to randomly choose symbol for player and computer.
+        Get Player's Coordinates in the form of row and column.
         """
-        num = randint(1,2)
-        return num
+        coords = []
+        while True:
+            try: row = int(input("Enter Row Number: "))
+            except ValueError: print("Invalid Input!")
+            else: 
+                if row not in self.valid_inputs: print("Select from 1 to 3.")
+                else: 
+                    coords.append(row)
+                    break
+        while True:
+            try: col = int(input("Enter Column Number: "))
+            except ValueError: print("Invalid Input!")
+            else: 
+                if col not in self.valid_inputs: print("Select from 1 to 3.")
+                else: 
+                    coords.append(col)
+                    break
+        return coords
+
+
+    def random_number(self):
+        """
+        This Function will generate random number,
+        from 1 to 2, to use for symbol.
+        """
+        number = randint(1, 2)
+        return number
+
+
+    def swap_player_turn(self, symbol):
+        """This function will Swap the Player's Turn."""
+        if symbol == 'X':
+            symbol = 'O'
+        elif symbol == 'O':
+            symbol = 'X'
+        return symbol
 
 
     def is_winner(self, player):
-        """Decide the winner."""
+        """This Functio will Decide the winner of TicTacToe."""
         n = len(self.board)
-        win = False
-        # Check Rows
-        for row in range(n):
+        win = None
+        # Check Rows.
+        for i in range(n):
             win = True
-            for col in range(n):
-                if self.board[row][col] != player:
+            for j in range(n):
+                if self.board[i][j] != player:
                     win = False
-                    break
             if win:
                 return win
-        
-        # Check Columns     
-        for row in range(n):
-            win = True      
-            for col in range(n):
-                if self.board[col][row] != player:
+        # Check Columns
+        for i in range(n):
+            win = True
+            for j in range(n):
+                if self.board[j][i] != player:
                     win = False
-                    break
             if win:
                 return win
-
-        # Check Diagnals    
-        win = True           
-        for i in range(n):  
-            if self.board[i][i] != player:
+        # Check Diagnals
+        win = True
+        for i in range (n):
+            if self.board != player:
                 win = False
         if win:
-            return win 
-
-        win = True
+            return win
+        
         for i in range(n):
-            if self.board[i][n - 1 - i] != player:
+            if self.board[n - 1 - i] != player:
                 win = False
         if win:
             return win
         return False
-
+        
 
     def start_game(self):
-        """Let the game Begin's."""
-        p_symbol = 'X' if self.random_turn() == 1 else 'O'
-        c_symbol = 'O' if p_symbol == 'X' else 'X'
+        """Let the game begin's."""
+        self.print_board()
+        print()
+        symbol = 'X' if self.random_number == 1 else 'O'
 
         while True:
-            # Get Player's coordinates
+            # If board's Full then it's a Draw(exit loop)
+            if len(self.possible_cords) == 0:
+                print("It's a Draw.")
+                break
+            print("Player '{0}' Turn.".format(symbol))
             player = self.player_cords()
+            # If Slot is Occupied.
             if player not in self.possible_cords:
-                print("That slot is already occupied, Try again!\n")
+                print("That Slot is already Occupied, Select Another Slot.\n")
             else:
+                # If slot is not Occupied remove it from possible coords.
                 self.possible_cords.remove(player)
-                # Update Player's coordinates on board.
-                self.board[player[0] - 1][player[1] - 1] = p_symbol
-                # Get Computer's coordinates.
-                computer = choice(self.possible_cords)
-                self.possible_cords.remove(computer)
-                # Update Computer's coordinates on board.
-                self.board[computer[0] - 1][computer[1] - 1] = c_symbol
+                # Draw symbol on board.
+                self.board[player[0] - 1][player[1] - 1] = symbol
+                # Print board.
                 print()
-                # Print board after Player's and Computer's Turn.
                 self.print_board()
                 print()
-                # If board is Full End the game.
-                if len(self.possible_cords) == 0:
-                    print("It's a Draw!")
+                # If Player 'O' or 'X' wins
+                # Print Message and exit loop.
+                if self.is_winner(symbol):
+                    print('Player {0} won!'.format(symbol))
                     break
-
-            # Check Winner
-            if self.is_winner(c_symbol):
-                print("Computer won! Better luck next time.")
-                break
-            elif self.is_winner(p_symbol):
-                print("You outsmarted computer, You won!")
-                break
+                # Swap Player's Turn.
+                symbol = self.swap_player_turn(symbol)
+            
 
 game = TicTacToe()
 game.start_game()
