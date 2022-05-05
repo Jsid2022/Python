22dc11552f1150f2254838519315a98d46b704b1
Siddharth <vasujain8998@gmail.com>
2022-05-05 15:28:51 +0530

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,55 +1,51 @@
+import time
 from random import randint
-
 
 def linear_search(target, list):
     """Search your target through Linear search."""
-    result = []
     for item in list:
         if item == target: 
-            result.append(item)
-            print("Your Number {0} is in the list.".format(item))
+            return True
+    return False
 
 def binary_search(target, list):
     """Search Your target through Binary search."""
     # Copy of Original List.
-    copy_list = sorted(list[:])
+    low = 0
+    high = len(list)
     while True:
-        # Update length of List in each iteration.
-        list_length = len(copy_list)
-
         # Get the middle index of list.
-        middle_index = list_length // 2
+        middle_index = (low + high) // 2
         # Item at middle index.
-        item = copy_list[middle_index]
+        item_at_mid = list[middle_index]
         
-        # If the middle_index is 0 and search is not item
-        # exit loop
-        if middle_index == 0 and target != item:
-            return -1
-        elif target == item:  # Item Found.(Can Exit loop)
-            print("Yes your Number is in list")
-            break
-        # if search is smaller at middle index's item
-        # remove all the items smaller than middle index's item.
-        elif target < item: 
-            copy_list = copy_list[:middle_index]
-        # else vice versa.
-        else:
-            copy_list = copy_list[middle_index:]
-
+        if item_at_mid == target:  # Item Found.(Can Exit loop)
+            return True
+        elif item_at_mid < target:  # If item at mid is smalled then tagret
+            low = middle_index + 1  # Look for the Upper half of in list
+        else:                       # else
+            high = middle_index     # Look for lower half in list.
 
 # Make a random list of 100 Integers.
 random_list1 = set()
-while len(random_list1) != 100:
+while len(random_list1) != 10000:
     random_list1.add(randint(1, 10000))
 
-# Make a list of random 10 integers.
-random_list2 = set()
-while len(random_list2) != 10:
-    random_list2.add(randint(1, 10000))
-
+# Sort list.
 sorted_list = sorted(list(random_list1))
-search_list = sorted(list(random_list2))
 
-for item in search_list:
-    linear_search(item, search_list)
+# Measure time for both searches.
+
+# Linear Seach
+start = time.process_time()
+for item in sorted_list:
+    linear_search(item, sorted_list)
+end = time.process_time()
+print("Linear Search Time: {0}".format(end - start))
+
+# Binary Search.
+start = time.process_time()
+for item in sorted_list:
+    binary_search(item, sorted_list)
+end = time.process_time()
+print("Binary Search Time: {0}".format(end - start))
